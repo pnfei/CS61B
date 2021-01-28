165498bbeacc5a09a39b2935cfd7f94fd7dd0af2
Tom <pengfeitong@gmail.com>
2021-01-27 23:56:12 -0700

    add test1 and test2

--- lab1/HelloWorld.java
+++ lab1/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
        public static void main(String[] args) {
             System.out.println("Hello world!");
+            System.out.printf("add test1");
+            System.out.printf("add test2");
        }
 }
